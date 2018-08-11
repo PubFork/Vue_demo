b3301cf9f0e9239304483acc85a8f40388bafc37
rw <405212760@qq.com>
2018-08-11 16:09:00 +0800

    first

---
+++ music_vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app" @touchmove.prevent>
+    <!-- 使用组件 -->
+    <m-header></m-header>
+    <tab></tab>
+    <!-- 包裹可以不用再次请求，直接进入到缓存中 -->
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+    <player></player>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  // 引入
+  import MHeader from 'components/m-header/m-header'
+  import Tab from 'components/tab/tab'
+  import Player from 'components/player/player'
+  // 注册组件
+  export default {
+    components: {
+      MHeader,
+      Tab,
+      Player
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
---
+++ music_vue/src/router/index.js
@@ -0,0 +1,117 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// import Recommend from 'components/recommend/recommend'
+// import Singer from 'components/singer/singer'
+// import Rank from 'components/rank/rank'
+// import Search from 'components/search/search'
+
+// json里面注释的内容
+//   "dev": "webpack-dev-server --inline --progress --config build/webpack.dev.conf.js",
+//   "start": "npm run dev",
+
+// "dev": "node build/dev-server.js",
+// "start": "node build/dev-server.js",
+
+Vue.use(Router)
+
+const Recommend = (resolve) => {
+  import('components/recommend/recommend').then((module) => {
+    resolve(module)
+  })
+}
+
+const Singer = (resolve) => {
+  import('components/singer/singer').then((module) => {
+    resolve(module)
+  })
+}
+
+const Rank = (resolve) => {
+  import('components/rank/rank').then((module) => {
+    resolve(module)
+  })
+}
+
+const Search = (resolve) => {
+  import('components/search/search').then((module) => {
+    resolve(module)
+  })
+}
+
+const SingerDetail = (resolve) => {
+  import('components/singer-detail/singer-detail').then((module) => {
+    resolve(module)
+  })
+}
+
+const Disc = (resolve) => {
+  import('components/disc/disc').then((module) => {
+    resolve(module)
+  })
+}
+
+const TopList = (resolve) => {
+  import('components/top-list/top-list').then((module) => {
+    resolve(module)
+  })
+}
+
+const UserCenter = (resolve) => {
+  import('components/user-center/user-center').then((module) => {
+    resolve(module)
+  })
+}
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/recommend'
+    },
+    {
+      path: '/recommend',
+      component: Recommend,
+      children: [
+        {
+          path: ':id',
+          component: Disc
+        }
+      ]
+    },
+    {
+      path: '/singer',
+      component: Singer,
+      children: [
+        {
+          path: ':id',
+          component: SingerDetail
+        }
+      ]
+    },
+    {
+      path: '/rank',
+      component: Rank,
+      children: [
+        {
+          path: ':id',
+          component: TopList
+        }
+      ]
+    },
+    {
+      path: '/search',
+      component: Search,
+      children: [
+        {
+          path: ':id',
+          component: SingerDetail
+        }
+      ]
+    },
+    {
+      path: '/user',
+      component: UserCenter
+    }
+  ]
+})
